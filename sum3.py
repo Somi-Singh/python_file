b37c2458e669b5713d6c9136cbc96ca3957cf592
somi singh <77184202+Somi-Singh@users.noreply.github.com>
2021-08-04 18:26:50 +0530

    Delete sum3.py

--- sum3.py
+++
@@ -1,9 +0,0 @@
-def averge():
-    sum=a+b+c
-    print(sum)
-    averge=sum//3
-    print(averge)
-a=int(input("enter a number"))
-b=int(input("enter a number"))
-c=int(input("enter a number"))
-averge()
