0eb9fa90ddf2df68838eacf5ab39e9301af52da9
somi singh <77184202+Somi-Singh@users.noreply.github.com>
2021-08-04 18:35:55 +0530

    Delete somi.py

--- somi.py
+++
@@ -1,9 +0,0 @@
-f = open("demofile2.txt", "w")
-f.write("Now the file has more content!")
-f.close()
-
-#open and read the file after the appending:
-f = open("demofile2.txt", "r")
-# a=f.read()
-# print(a)
-print(f.read())
