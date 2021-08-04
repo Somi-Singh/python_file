5f4f681ce2f05d771453abb62e1e115c10992860
somi singh <77184202+Somi-Singh@users.noreply.github.com>
2021-08-04 18:29:06 +0530

    Delete attendance.py

--- attendance.py
+++
@@ -1,7 +0,0 @@
-def attendance(name,status="absent"):
-    print(name,"is",status," today")
-
-attendance("kartik","present")
-attendance("sonu")
-attendance("vishal","present")
-attendance("umesh")
