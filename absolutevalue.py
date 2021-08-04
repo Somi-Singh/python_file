c449c853b4b06d9cdc17df8a276bc5f795c865e2
somi singh <77184202+Somi-Singh@users.noreply.github.com>
2021-08-04 18:28:17 +0530

    Delete absolutevalue.py

--- absolutevalue.py
+++
@@ -1,8 +0,0 @@
-def absolutevalue(num):
-    '''this function returns the abs_value of the entered number'''
-    if num>=0:
-        return num
-    else:
-        return -num
-print(absolutevalue(2))
-print(absolutevalue(-4))
