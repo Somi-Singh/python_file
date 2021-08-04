84a9df4e6f6a56f0015ec47a9ad3bd91f774c4b8
somi singh <77184202+Somi-Singh@users.noreply.github.com>
2021-08-04 18:34:44 +0530

    Delete print lines2.py

--- print lines2.py
+++
@@ -1,8 +0,0 @@
-def function_say_bye():
-    print ("Aapko mil ke maza aaya. ")
-    print ("Bye bye")
-function_say_bye()
-function_say_bye()
-print ("Python ka istamaal bahot jagah hota hai.")
-function_say_bye()
-function_say_bye()
